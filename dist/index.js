5352763a033c6020c6ac9cde45b8cee78c2fe999
Himanshu <himanshu@sahusoft.com>
2017-02-15 13:38:22 +0530

    remove unnecessary functions

--- dist/index.js
+++ dist/index.js
@@ -28,47 +28,12 @@ exports.default = function (authConfig) {
       return app.get([serviceName, 'accessToken']);
     };
 
-    var login = function login() {
-      var credentials = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
-
-      return new Promise(function (resolve, reject) {
-        setTimeout(function () {
-          setUser({
-            name: 'Himanshu',
-            email: 'himanshu@sahusoft.com'
-          });
-          setToken('testToken');
-          app.http.setDefaultHeaders({
-            Authorization: 'Bearer ' + 'testToken'
-          });
-          resolve({
-            user: getUser(),
-            token: getToken()
-          });
-        }, 1000);
-      });
-    };
-
-    var logout = function logout() {
-      var credentials = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
-
-      return new Promise(function (resolve, reject) {
-        setTimeout(function () {
-          setUser(null);
-          setToken(null);
-          resolve('success');
-        }, 1000);
-      });
-    };
-
     var authService = {
       setUser: setUser,
       getUser: getUser,
       user: user,
       setToken: setToken,
-      getToken: getToken,
-      login: login,
-      logout: logout
+      getToken: getToken
     };
 
     app.set(serviceName, authService);
--- src/index.js
+++ src/index.js
@@ -23,43 +23,12 @@ export default function(authConfig) {
       return app.get([serviceName, 'accessToken']);
     };
 
-    const login = (credentials = {}) => {
-      return new Promise(function(resolve, reject) {
-        setTimeout(() => {
-          setUser({
-            name: 'Himanshu',
-            email: 'himanshu@sahusoft.com',
-          });
-          setToken('testToken');
-          app.http.setDefaultHeaders({
-            Authorization: `Bearer ${'testToken'}`
-          })
-          resolve({
-            user: getUser(),
-            token: getToken(),
-          });
-        }, 1000);
-      });
-    };
-
-    const logout = (credentials = {}) => {
-      return new Promise(function(resolve, reject) {
-        setTimeout(() => {
-          setUser(null);
-          setToken(null);
-          resolve('success');
-        }, 1000);
-      });
-    };
-
     const authService = {
       setUser,
       getUser,
       user,
       setToken,
       getToken,
-      login,
-      logout,
     };
 
     app.set(serviceName, authService);
